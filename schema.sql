722e8d0339c3ed133fb6f4d45831a2df76ae8a7a
Eduardo Urbaez <eurbaezjr@gmail.com>
2019-12-31 20:19:50 -0400

    updated schema

--- schema.sql
+++ schema.sql
@@ -5,12 +5,12 @@ USE groceries_db;
 
 CREATE TABLE groceries (
   id INT NOT NULL AUTO_INCREMENT,
-  unit_name VARCHAR(80) NULL,
+  unit_name VARCHAR (80) NULL,
   quantity INT NULL,
   unit_price DECIMAL (65,2) NULL,
   unit_profit DECIMAL (65,2) NULL,
   unit_revenue DECIMAL (65,2) NULL,
-  unit_image VARCHAR(120) NULL,
+  unit_image VARCHAR (120) NULL,
   PRIMARY KEY (id)
 );
 
